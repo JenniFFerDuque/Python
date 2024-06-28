dc46294af98df94d641f7939d19ef48d3544b757
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-06-28 12:53:21 -0500

    How to use print function

---
+++ Basic/print.py
@@ -0,0 +1,13 @@
+#Print text:
+print("Hello world!")
+
+#Print math operations:
+print(2+20)
+print(5*10)
+print(5/4)
+
+#Concatenate text:
+Name = "Jenniffer"
+Age = 18
+print("Mi name is:", Name, "and Im:", Age)
+#It just necessary put in the middle a comma
