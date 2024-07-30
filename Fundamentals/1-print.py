78dcbcf8970d48ee1b2652e3778ad3a658f256cf
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-07-30 12:22:02 -0500

    docs(fundamentals): print function
    - How to use it
    - \n
    - \t
    - Examples

---
+++ Fundamentals/1-print.py
@@ -0,0 +1,13 @@
+# Basic print function
+print("Hello world")
+
+# \n - new line
+print("Hello \nworld")
+
+# \t - tab
+print("Hello \tworld")
+
+#Examples:
+print("* \n** \n***")
+print("Himanshu \n\tDAVV \n\t\tIndore")
+print("\t* \n* \t\t* \n\t*")
