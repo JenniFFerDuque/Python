944ae8ffd5ad3b581ce2300bb5e81b80b912984f
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-07-31 11:57:04 -0500

    docs(fundamentals): operators
    - Arithmetic operators (+, -, /, //, %, **)
    - Relational operators (<, >, <=, >=, ==, !=)
    - Boolean operators (True, False)
    - Logical operators (not, or, and)
    - Assignation operators (=, +=, -=, /=, //=, **=)

---
+++ Fundamentals/4-operators.py
@@ -0,0 +1,45 @@
+# Operators:
+
+# Arithmetic operators:
+print(7 % 2) #Module
+print(10 / 3) #Normal division
+print(10 // 3) #Rounded division
+print(3 ** 2) #Exponents
+print(2 + 3) #Addition
+print(2 - 3) #Sustraction
+print(2 * 3) #Multiplication
+
+# Relational operators (it returns True or False):
+print(10 < 5)
+print(10 > 5)
+print(10 <= 5)
+print(10 >= 5)
+print(2 == 2)
+print(3 != 4)
+
+# Boolean operators: Capitalizing first letter is important
+""" True == 1
+False == 0 """
+
+# Logical operators:
+print(not True)
+print(not False)
+print(not(10 == 10))
+print(True and True)
+print(True or False)
+
+# Assignation operations:
+number = 10
+print(number)
+
+number += 5 #number = number + 5
+print(number)
+
+number -= 5 #number = number - 5
+print(number)
+
+number //= 5 #number = number / 5
+print(number)
+
+number **= 2 #number = number ** 2
+print(number)
