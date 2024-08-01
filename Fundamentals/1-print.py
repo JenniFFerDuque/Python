eff4011824a79f99751f5bd7026bc15797c7fbaf
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-08-01 13:03:54 -0500

    feat(print function): add wider information and examples
    - sep
    - end
    - f-string
    - Print a specific amount of digits

--- Fundamentals/1-print.py
+++ Fundamentals/1-print.py
@@ -11,3 +11,19 @@ print("Hello \tworld")
 print("* \n** \n***")
 print("Himanshu \n\tDAVV \n\t\tIndore")
 print("\t* \n* \t\t* \n\t*")
+
+# sep: it allows you specify how we'll separate the words
+print("Hello", "World", "I'm Jenni", sep="; ")
+
+# end: it allows you printing in the same line the next 'print', it's the contrary of \n
+name = "Jenniffer"
+last_name = "Duque"
+print(name, end=" ")
+print(last_name)
+
+# f-strings: it allows you arrange the information in a better way
+print(f"Name: {name}, lastname: {last_name}") #variables puts always on '{}'
+
+#Format: it allows you print an specific amount of digits
+value = 3.14167
+print("Value: {:.2f}".format(value))
