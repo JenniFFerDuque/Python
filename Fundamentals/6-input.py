0e07001ce49b7f9ad8240505a305b57159696a0a
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-08-02 12:45:02 -0500

    docs(fundamentals): input function
    - How to use input function
    - Print the vars values
    - Casting: all values returned by input function are string type

---
+++ Fundamentals/6-input.py
@@ -0,0 +1,16 @@
+''' User imput: The information input by user always will be a string type. To change that
+must be necessary use (casting) sentence '''
+
+#Examples: 
+name = input("Enter you name: ")
+print("Name:", name) #Using normal print
+
+age = input("Enter your age: ")
+print(f"Age: {age}") #using f-string (more fancy)
+
+# Casting: 
+phone = int(input("Enter you phone number updated: "))
+print("Phone:", phone)#Must use normal print when casting
+
+savings = float(input("Enter how much savings do you have: "))
+print ("Savings:", savings)
