b78251d72c04eaeb74582e4536ccc8033ac3c996
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-08-02 13:16:02 -0500

    project(fundamentals): project_01
    -   Enter 2 numbers from user and print sum of them.
    
    In practice: Print, input, casting, using variables, math operators.

---
+++ Fundamentals/project_01.py
@@ -0,0 +1,7 @@
+# First program:
+
+# 1. Enter 2 numbers from user and print sum of them.
+number_1 = float(input("Enter your first number: "))
+number_2 = float(input("Enter your second number: "))
+
+print("Sum of them:", (number_1 + number_2))
