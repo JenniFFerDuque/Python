2867140072efad1712b861ed5c17413c612b7485
Jenniffer Duque <jenniffer.zamora0608@gmail.com>
2024-08-05 11:35:43 -0500

    feat(input function): add structure to use it
    - var = casting(input(message))
    - casting is used to specify if it isn't a string

--- Fundamentals/6-input.py
+++ Fundamentals/6-input.py
@@ -1,6 +1,8 @@
 ''' User imput: The information input by user always will be a string type. To change that
 must be necessary use (casting) sentence '''
 
+# Structure: var = casting(input("message"))
+
 #Examples: 
 name = input("Enter you name: ")
 print("Name:", name) #Using normal print
